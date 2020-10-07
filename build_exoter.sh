e8a23b1673eb2642653c2232cfc3205f41160e4d
Tim Wiese <tim.wiese@esa.int>
2020-10-07 19:00:49 +0200

    Pass through build script arguments to docker build commands

--- build_exoter.sh
+++ build_exoter.sh
@@ -2,6 +2,6 @@
 
 DIR=$(dirname $0)
 
-docker build --file $DIR/DockerfileBase --tag esa-prl/rock-base $DIR && \
-docker build --file $DIR/DockerfileRover --tag esa-prl/rock-rover $DIR && \
-docker build --file $DIR/DockerfileExoter --tag esa-prl/rock-exoter $DIR
+docker build "$@" --file $DIR/DockerfileBase --tag esa-prl/rock-base $DIR && \
+docker build "$@" --file $DIR/DockerfileRover --tag esa-prl/rock-rover $DIR && \
+docker build "$@" --file $DIR/DockerfileExoter --tag esa-prl/rock-exoter $DIR
