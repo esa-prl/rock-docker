7f686f591fd4850fba59a907bf6ac35fd56d5156
Levin Gerdes <18442515+levingerdes@users.noreply.github.com>
2020-11-02 20:35:36 +0100

    Remove image tags using this repo's branch names

--- build_kaniko.sh
+++ build_kaniko.sh
@@ -10,13 +10,8 @@ CI_REGISTRY_PASSWORD=$6
 CI_PROJECT_DIR=$7
 docker_image_name=$8
 
-if [ "$CI_COMMIT_REF_NAME" = "$CI_DEFAULT_BRANCH" ]; then
-   docker_tagged_name="$docker_image_name:latest"
-else
-   docker_tagged_name="$docker_image_name:$CI_COMMIT_REF_NAME"
-fi
-
 echo "Preparing Kaniko Build Context to build Docker images"
 echo "{\"auths\":{\"$CI_REGISTRY\":{\"username\":\"$CI_REGISTRY_USER\",\"password\":\"$CI_REGISTRY_PASSWORD\"}}}" > /kaniko/.docker/config.json
-/kaniko/executor --context "$CI_PROJECT_DIR" --dockerfile "$CI_PROJECT_DIR"/"$docker_image_name".dockerfile --destination "$CI_REGISTRY_IMAGE/$docker_tagged_name"
+
+/kaniko/executor --context "$CI_PROJECT_DIR" --dockerfile "$CI_PROJECT_DIR"/"$docker_image_name".dockerfile --destination "$CI_REGISTRY_IMAGE/$docker_image_name"
 
